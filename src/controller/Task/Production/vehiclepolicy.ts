e5c8310457c388367192fb74a91f470e92421108
charrlesss <charlespalencia21@gmail.com>
2023-10-18 17:25:17 +0800

    vpolicy

--- src/controller/Task/Production/vehiclepolicy.ts
+++ src/controller/Task/Production/vehiclepolicy.ts
@@ -7,8 +7,22 @@ import {
   getRates,
   getMortgagee,
   getTPL_IDS,
+  findPolicy,
+  getRate,
+  getClientById,
+  deleteJournalBySource,
+  deleteVehiclePolicy,
+  deletePolicy,
+  createPolicy,
+  createVehiclePolicy,
+  createJournal,
+  createJournalInVP,
+  updateJournalByPolicy,
+  getTempPolicyID,
+  searchDataVPolicy,
 } from "../../../model/Task/Production/vehicle-policy";
 import promiseAll from "../../../lib/promise-all";
+import { create } from "domain";
 const VehiclePolicy = express.Router();
 
 VehiclePolicy.get(
@@ -19,6 +33,7 @@ VehiclePolicy.get(
         getClients(""),
         getAgents(""),
         getSubAccount(),
+        getTempPolicyID(),
         getPolicyAccount("COM"),
         getPolicyAccount("TPL"),
         getRates("COM"),
@@ -30,6 +45,7 @@ VehiclePolicy.get(
           clients,
           agents,
           sub_account,
+          tempPolicyId,
           com,
           tpl,
           rateCom,
@@ -44,6 +60,7 @@ VehiclePolicy.get(
               clients,
               agents,
               sub_account,
+              tempPolicyId,
               policy_account: {
                 com,
                 tpl,
@@ -100,4 +117,325 @@ VehiclePolicy.get(
   }
 );
 
+VehiclePolicy.post("/tpl-add-vehicle-policy", async (req, res) => {
+  const {
+    form_action,
+    form_type,
+    sub_account,
+    client_id,
+    client_name,
+    client_address,
+    agent_id,
+    agent_name,
+    agent_com,
+    PolicyAccount,
+    PolicyNo,
+    CCN,
+    ORN,
+    DateFrom,
+    DateTo,
+    DateIssued,
+    Model,
+    Make,
+    TB,
+    Color,
+    BLTFileNo,
+    PlateNo,
+    ChassisNo,
+    MotorNo,
+    AuthorizedCapacity,
+    UnladenWeigth,
+    TplType,
+    PremiumPaid,
+    EVSV,
+    Aircon,
+    Stereo,
+    Magwheels,
+    OthersRate,
+    OthersDesc,
+    CompreType,
+    Deductible,
+    Towing,
+    ARL,
+    BodyInjury,
+    PropertyDamage,
+    PersinalAccident,
+    Denomination,
+    Mortgagee,
+    MortgageeForm,
+    SectionI_II,
+    SectionIII,
+    OwnDamage,
+    Theft,
+    SectionIVA,
+    SectionIVB,
+    PremiumOther,
+    AOG,
+    AOGPercent,
+    TotalPremium,
+    Vat,
+    DocStamp,
+    LocalGovTax,
+    StradCom,
+    TotalDue,
+    Type,
+    LocalGovTaxPercent,
+    rateCost,
+    Source_No_Ref_ID,
+  } = req.body;
+  try {
+    if (await findPolicy(PolicyNo)) {
+      return res.send({
+        message: "Unable to save! Policy No. already exists!",
+        success: false,
+      });
+    }
+
+    //get Commision rate
+    const rate = (
+      (await getRate(PolicyAccount, "Vehicle", Denomination)) as Array<any>
+    )[0];
+    if (rate == null) {
+      return res.send({
+        message: "Please setup commission rate for this account and Line",
+        success: false,
+      });
+    }
+
+    const subAccount = ((await getClientById(client_id)) as Array<any>)[0];
+    const strArea =
+      subAccount.Acronym === "" ? sub_account : subAccount.Acronym;
+    const cStrArea = subAccount.ShortName;
+
+    //delete policy
+    await deletePolicy(sub_account, form_type, PolicyNo);
+    //delete v policy
+    await deleteVehiclePolicy(sub_account, form_type, PolicyNo);
+    //delete journal
+    await deleteJournalBySource(PolicyNo, "PL");
+
+    // insert policy
+    await createPolicy({
+      IDNo: client_id,
+      Account: PolicyAccount,
+      SubAcct: sub_account,
+      PolicyType: form_type,
+      PolicyNo: PolicyNo,
+      DateIssued,
+      TotalPremium: parseFloat(parseFloat(TotalPremium).toFixed(2)),
+      Vat,
+      DocStamp,
+      FireTax: "0",
+      LGovTax: LocalGovTax,
+      Notarial: "0",
+      Misc: StradCom,
+      TotalDue,
+      TotalPaid: "0",
+      Journal: false,
+      AgentID: agent_id,
+      AgentCom: agent_com,
+    });
+    // insert vehicle policy
+    await createVehiclePolicy({
+      PolicyNo,
+      Account: PolicyAccount,
+      PolicyType: form_type,
+      CoverNo: CCN,
+      ORNo: ORN,
+      DateFrom,
+      DateTo,
+      Model,
+      Make,
+      BodyType: TB,
+      Color,
+      BLTFileNo,
+      PlateNo,
+      ChassisNo,
+      MotorNo,
+      AuthorizedCap: AuthorizedCapacity,
+      UnladenWeight: UnladenWeigth,
+      TPL: "",
+      TPLLimit: "0.00",
+      PremiumPaid,
+      EstimatedValue: EVSV,
+      Aircon,
+      Stereo,
+      Magwheels,
+      Others: "",
+      OthersAmount: OthersRate,
+      Deductible,
+      Towing,
+      RepairLimit: ARL,
+      BodilyInjury: BodyInjury,
+      PropertyDamage,
+      PersonalAccident: PersinalAccident,
+      SecI: SectionI_II,
+      SecIIPercent: SectionIII,
+      ODamage: OwnDamage,
+      Theft,
+      Sec4A: SectionIVA,
+      Sec4B: SectionIVB,
+      Sec4C: PremiumOther,
+      AOG: AOG,
+      MortgageeForm: JSON.parse(Mortgagee),
+      Mortgagee: MortgageeForm,
+      Denomination,
+    });
+
+    if (!PolicyNo.includes("TP-")) {
+      await createJournalInVP({
+        Branch_Code: sub_account,
+        Date_Entry: DateIssued,
+        Source_Type: "PL",
+        Source_No: PolicyNo,
+        Explanation: `${form_type} Production`,
+        GL_Acct: "1.03.01",
+        Sub_Acct: strArea,
+        ID_No: PolicyNo,
+        cGL_Acct: "Premium Receivable",
+        cSub_Acct: cStrArea,
+        cID_No: client_name,
+        Debit: parseFloat(TotalDue),
+        Credit: 0,
+        TC: "P/R",
+        Remarks: "",
+        Source_No_Ref_ID,
+      });
+    } else {
+      await createJournalInVP({
+        Branch_Code: sub_account,
+        Date_Entry: DateIssued,
+        Source_Type: "PL",
+        Source_No: PolicyNo,
+        Explanation: `${form_type} Production`,
+        GL_Acct: "1.03.03",
+        Sub_Acct: strArea,
+        ID_No: PolicyNo,
+        cGL_Acct: "Premium Receivable",
+        cSub_Acct: cStrArea,
+        cID_No: client_name,
+        Debit: parseFloat(TotalDue),
+        Credit: 0,
+        TC: "P/R",
+        Remarks: "",
+        Source_No_Ref_ID,
+      });
+    }
+
+    if (!PolicyNo.includes("TP-")) {
+      await createJournalInVP({
+        Branch_Code: sub_account,
+        Date_Entry: DateIssued,
+        Source_Type: "PL",
+        Source_No: PolicyNo,
+        Explanation: `${form_type} Production`,
+        GL_Acct: "4.02.01",
+        Sub_Acct: strArea,
+        ID_No: PolicyNo,
+        cGL_Acct: "A/P",
+        cSub_Acct: cStrArea,
+        cID_No: client_name,
+        Debit: 0,
+        Credit: parseFloat(TotalDue),
+        TC: "A/P",
+        Remarks: "",
+        Source_No_Ref_ID,
+      });
+    } else {
+      await createJournalInVP({
+        Branch_Code: sub_account,
+        Date_Entry: DateIssued,
+        Source_Type: "PL",
+        Source_No: PolicyNo,
+        Explanation: `${form_type} Production`,
+        GL_Acct: "4.02.07",
+        Sub_Acct: strArea,
+        ID_No: PolicyNo,
+        cGL_Acct: "A/P",
+        cSub_Acct: cStrArea,
+        cID_No: client_name,
+        Debit: 0,
+        Credit: parseFloat(TotalDue),
+        TC: "A/P",
+        Remarks: "",
+        Source_No_Ref_ID,
+      });
+    }
+    if (form_action === "REG" && form_type === "TPL") {
+      await updateJournalByPolicy(PolicyNo, "CTPL Registration");
+      await createJournalInVP({
+        Branch_Code: sub_account,
+        Date_Entry: DateIssued,
+        Source_Type: "GL",
+        Source_No: PolicyNo,
+        Explanation: `${form_type} Production`,
+        GL_Acct: "4.02.01",
+        Sub_Acct: strArea,
+        ID_No: PolicyNo,
+        cGL_Acct: "A/P",
+        cSub_Acct: cStrArea,
+        cID_No: client_name,
+        Debit: parseFloat(TotalDue),
+        Credit: 0,
+        TC: "P/R",
+        Remarks: "",
+        Source_No_Ref_ID,
+      });
+      await createJournalInVP({
+        Branch_Code: sub_account,
+        Date_Entry: DateIssued,
+        Source_Type: "GL",
+        Source_No: PolicyNo,
+        Explanation: `${form_type} Production`,
+        GL_Acct: "4.02.01",
+        Sub_Acct: strArea,
+        ID_No: PolicyNo,
+        cGL_Acct: "CTPL Inventory",
+        cSub_Acct: cStrArea,
+        cID_No: client_name,
+        Debit: 0,
+        Credit: parseFloat(TotalDue),
+        TC: "CTI",
+        Remarks: "",
+        Source_No_Ref_ID,
+      });
+      await createJournalInVP({
+        Branch_Code: sub_account,
+        Date_Entry: DateIssued,
+        Source_Type: "GL",
+        Source_No: PolicyNo,
+        Explanation: `${form_type} Production`,
+        GL_Acct: "4.02.01",
+        Sub_Acct: strArea,
+        ID_No: PolicyNo,
+        cGL_Acct: "CTPL Income",
+        cSub_Acct: cStrArea,
+        cID_No: client_name,
+        Debit: 0,
+        Credit: parseFloat(TotalDue) - parseFloat(rateCost),
+        TC: "CIN",
+        Remarks: "",
+        Source_No_Ref_ID,
+      });
+    }
+    res.send({ message: "Create Journal Successfully", success: true });
+  } catch (err: any) {
+    res.send({ message: err.message, success: false });
+  }
+});
+
+VehiclePolicy.get("/tpl-search-vehicle-policy", async (req, res) => {
+  const { form_type, form_action, search } = req.query;
+  const getSearch = await searchDataVPolicy(
+    search as string,
+    form_type as string,
+    (form_action as string) === "TEMP"
+  );
+
+  res.send({
+    message: "Search Successfully",
+    success: false,
+    searchVPolicy: getSearch,
+  });
+});
 export default VehiclePolicy;
--- src/model/Task/Production/vehicle-policy.ts
+++ src/model/Task/Production/vehicle-policy.ts
@@ -101,7 +101,8 @@ export async function getTPL_IDS() {
   return await prisma.$queryRawUnsafe(`
   SELECT 
       MIN(Source_No) AS Source_No,
-      MIN(CAST(Credit AS DECIMAL (18 , 2 ))) as Cost
+      MIN(CAST(Credit AS DECIMAL (18 , 2 ))) as Cost ,
+      Source_No_Ref_ID
   FROM
       upward.journal
   WHERE
@@ -165,3 +166,316 @@ export async function updateJournal(
     },
   });
 }
+
+export async function findPolicy(PolicyNo: string) {
+  return await prisma.policy.findUnique({ where: { PolicyNo } });
+}
+export async function getPolicy(
+  account: string,
+  form_type: string,
+  policy_no: string
+) {
+  const query = `
+  SELECT * FROM upward.policy 
+  WHERE 
+  Account = '${account}'
+  AND PolicyType = '${form_type}' 
+  AND PolicyNo = '${policy_no}'
+  `;
+  return await prisma.$queryRawUnsafe(query);
+}
+export async function getRate(account: string, line: string, type: string) {
+  const query = `
+  select Rate from Rates 
+  where 
+  Account = '${account}' 
+  and Line = '${line}' 
+  and Type = '${type}'
+  `;
+  return await prisma.$queryRawUnsafe(query);
+}
+
+export async function getClientById(entry_client_id: string) {
+  const query = `
+  SELECT 
+    b.*
+  FROM 
+  upward.entry_client a
+    LEFT JOIN
+  upward.sub_account b ON a.sub_account = b.Sub_Acct
+  where a.entry_client_id ='${entry_client_id}'
+  `;
+  return await prisma.$queryRawUnsafe(query);
+}
+
+export async function deletePolicy(
+  subAccount: string,
+  form_type: string,
+  policyNo: string
+) {
+  const query = `
+  delete from upward.policy 
+  where 
+  Account = '${subAccount}' 
+  and PolicyType = '${form_type}' 
+  and PolicyNo = '${policyNo}'
+  `;
+  return await prisma.$queryRawUnsafe(query);
+}
+
+export async function deleteVehiclePolicy(
+  subAccount: string,
+  form_type: string,
+  policyNo: string
+) {
+  const query = `
+  delete from upward.vpolicy 
+  where 
+  Account = '${subAccount}' 
+  and PolicyType = '${form_type}' 
+  and PolicyNo = '${policyNo}'
+  `;
+  return await prisma.$queryRawUnsafe(query);
+}
+
+export async function deleteJournalBySource(
+  source_no: string,
+  source_type: string
+) {
+  const query = `
+  delete from upward.journal 
+  where 
+  Source_No = '${source_no}' 
+  and Source_Type = '${source_type}'
+  `;
+  return await prisma.$queryRawUnsafe(query);
+}
+
+export async function createPolicy(data: {
+  IDNo: string;
+  Account: string;
+  SubAcct: string;
+  PolicyType: string;
+  PolicyNo: string;
+  DateIssued: Date;
+  TotalPremium: number;
+  Vat: string;
+  DocStamp: string;
+  FireTax: string;
+  LGovTax: string;
+  Notarial: string;
+  Misc: string;
+  TotalDue: string;
+  TotalPaid: string;
+  Journal: boolean;
+  AgentID: string;
+  AgentCom: string;
+}) {
+  return await prisma.policy.create({
+    data,
+  });
+}
+
+export async function createVehiclePolicy(data: {
+  PolicyNo: string;
+  Account: string;
+  PolicyType: string;
+  CoverNo: string;
+  ORNo: string;
+  DateFrom: string;
+  DateTo: string;
+  Model: string;
+  Make: string;
+  BodyType: string;
+  Color: string;
+  BLTFileNo: string;
+  PlateNo: string;
+  ChassisNo: string;
+  MotorNo: string;
+  AuthorizedCap: string;
+  UnladenWeight: string;
+  TPL: string;
+  TPLLimit: string;
+  PremiumPaid: string;
+  EstimatedValue: string;
+  Aircon: string;
+  Stereo: string;
+  Magwheels: string;
+  Others: string;
+  OthersAmount: string;
+  Deductible: string;
+  Towing: string;
+  RepairLimit: string;
+  BodilyInjury: string;
+  PropertyDamage: string;
+  PersonalAccident: string;
+  SecI: string;
+  SecIIPercent: string;
+  ODamage: string;
+  Theft: string;
+  Sec4A: string;
+  Sec4B: string;
+  Sec4C: string;
+  AOG: string;
+  MortgageeForm: boolean;
+  Mortgagee: string;
+  Denomination: string;
+}) {
+  return await prisma.vpolicy.create({
+    data,
+  });
+}
+
+export async function createJournalInVP(data: {
+  Branch_Code: string;
+  Date_Entry: string;
+  Source_Type: string;
+  Source_No: string;
+  Explanation: string;
+  GL_Acct: string;
+  Sub_Acct: string;
+  ID_No: string;
+  cGL_Acct: string;
+  cSub_Acct: string;
+  cID_No: string;
+  Debit: number;
+  Credit: number;
+  TC: string;
+  Remarks: string;
+  Source_No_Ref_ID: string;
+}) {
+  return await prisma.journal.create({
+    data,
+  });
+}
+
+export async function updateJournalByPolicy(
+  Source_No: string,
+  Explanation: string
+) {
+  return await prisma.journal.updateMany({
+    where: {
+      Source_No,
+      AND: {
+        Explanation,
+      },
+    },
+    data: {
+      Remarks: "Used",
+    },
+  });
+}
+
+export async function getTempPolicyID() {
+  return await prisma.$queryRawUnsafe(`
+  select
+  concat(
+  'TP-',
+  right('000000',6 - LENGTH(CAST(CAST(substring(a.PolicyNo,4) as SIGNED) + 1 As SIGNED))),
+  IF(
+      COALESCE(a.PolicyNo, '') = '' OR policyNo IS NULL,
+      '000001',
+    CAST(substring(a.PolicyNo,4) as SIGNED) + 1
+    )
+  ) AS tempPolicy_No
+   from (
+    SELECT  MAX(PolicyNo) as PolicyNo FROM upward.vpolicy a where left(a.PolicyNo ,2) = 'TP' and a.PolicyType = 'COM' ORDER BY a.PolicyNo ASC
+  ) a`);
+}
+
+export async function searchDataVPolicy(
+  search: string,
+  policyType: string,
+  isTemp: boolean
+) {
+  return await prisma.$queryRawUnsafe(`
+      SELECT 
+      a.*,
+      b.*,
+      concat(c.firstname,', ',c.middlename,', ',c.lastname) as client_fullname,
+      c.address as address,
+      concat(d.firstname,', ',d.middlename,', ',d.lastname) as agent_fullname
+    FROM
+      upward.policy a
+          LEFT JOIN
+        upward.vpolicy b ON a.PolicyNo = b.PolicyNo
+        left join upward.entry_client c on a.IDNo = c.entry_client_id 
+        left join upward.entry_agent d on a.AgentID = d.entry_agent_id 
+            WHERE 
+        a.PolicyType = '${policyType}' and
+       ${isTemp ? "left(a.PolicyNo,3) = 'TP-'and" : "left(a.PolicyNo,3) != 'TP-' and"}
+        a.PolicyNo like '%${search}%' and
+        c.firstname like '%${search}%' and 
+        c.lastname like '%${search}%'
+    ORDER BY a.DateIssued desc
+    LIMIT 100 
+  `);
+}
+
+
+        // //Vehicle policy
+        // PolicyAccount: "",
+        // PolicyNo: "",
+        // CCN: "",
+        // ORN: "",
+        // rateCost: "",
+        // //Period Insurance
+        // DateFrom: new Date(),
+        // DateTo: new Date(),
+        // DateIssued: new Date(),
+        // //Insured Unit
+        // Model: "charles1",
+        // Make: "",
+        // TB: "",
+        // Color: "",
+        // BLTFileNo: "",
+        // PlateNo: "",
+        // ChassisNo: "",
+        // MotorNo: "",
+        // AuthorizedCapacity: "",
+        // UnladenWeigth: "",
+      
+        // //==========================
+        // //tpl
+        // TplType: JSON.stringify({ Account: "", PremuimPaid: "0.00" }),
+        // PremiumPaid: "0.00",
+        // //compre
+        // EVSV: "0.00",
+        // Aircon: "0.00",
+        // Stereo: "0.00",
+        // Magwheels: "0.00",
+        // OthersRate: "0.00",
+        // OthersDesc: "",
+        // CompreType: "",
+      
+        // Deductible: "0.00",
+        // Towing: "0.00",
+        // ARL: "0.00",
+        // BodyInjury: "0.00",
+        // PropertyDamage: "0.00",
+        // PersinalAccident: "0.00",
+        // Denomination: "",
+      
+        // //==========================
+        // //mortgage
+        // Mortgagee: "false",
+        // MortgageeForm: "",
+        // //Premiums
+        // SectionI_II: "0.00",
+        // SectionIII: "0.00",
+        // OwnDamage: "0.00",
+        // Theft: "0.00",
+        // SectionIVA: "0.00",
+        // SectionIVB: "0.00",
+        // PremiumOther: "0.00",
+        // AOG: "0.00",
+        // AOGPercent: "0.00",
+        // TotalPremium: "0.00",
+        // Vat: "0.00",
+        // DocStamp: "0.00",
+        // LocalGovTaxPercent: "0.75",
+        // LocalGovTax: "0.00",
+        // StradCom: "0.00",
+        // TotalDue: "0.00",
+        // Type: "charles1",
+        // Source_No_Ref_ID: "",
