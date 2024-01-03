f78392875537a60ce62042ff7b04477314440ce8
charrlesss <charlespalencia21@gmail.com>
2024-01-03 17:12:08 +0800

    f

--- src/controller/Task/Accounting/return-checks.ts
+++ src/controller/Task/Accounting/return-checks.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import {
   GenerateReturnCheckID,
+  addNewReturnCheck,
+  deleteReturnCheck,
   getBranchName,
   getCheckList,
   getCreditOnSelectedCheck,
   getDebitOnSelectedCheck,
+  updateRCID,
+  updatePDCFromReturnCheck,
+  updateJournalFromReturnCheck,
+  deleteJournalFromReturnCheck,
+  addJournalFromReturnCheck,
 } from "../../../model/Task/Accounting/return-checks.model";
 const ReturnCheck = express.Router();
 
@@ -12,7 +19,7 @@ ReturnCheck.get("/get-new-return-check-id", async (req, res) => {
   try {
     res.send({
       message: "Successfully Get New Return Check ID.",
-      success: false,
+      success: true,
       returnCheckID: await GenerateReturnCheckID(),
     });
   } catch (error: any) {
@@ -23,7 +30,7 @@ ReturnCheck.get("/get-check-list", async (req, res) => {
   try {
     res.send({
       message: "Successfully Get Check List",
-      success: false,
+      success: true,
       checkList: await getCheckList(req.query.checkListSearch as string),
     });
   } catch (error: any) {
@@ -34,12 +41,82 @@ ReturnCheck.post("/get-modal-return-check-data", async (req, res) => {
   try {
     res.send({
       message: "Successfully Get Modal Data",
-      success: false,
+      success: true,
       branchName: await getBranchName(),
       credit: await getCreditOnSelectedCheck(req.body.BankAccount),
       debit: await getDebitOnSelectedCheck(req.body.Official_Receipt),
     });
   } catch (error: any) {
+    res.send({ message: error.message, success: false, credit: [], debit: [] });
+  }
+});
+ReturnCheck.post("/add-return-check", async (req, res) => {
+  try {
+    await deleteReturnCheck(req.body.RefNo);
+    req.body.selected.forEach(async (items: any, index: number) => {
+      await addNewReturnCheck({
+        Area: req.body.BranchCode,
+        RC_Date: req.body.DateReturn,
+        RC_No: req.body.RefNo,
+        Explanation: req.body.Explanation,
+        Check_No: items.Check_No,
+        Date_Deposit: new Date(items.Check_Date),
+        Amount: parseFloat(items.Amount.replace(/,/g, "")).toFixed(2),
+        Reason: items.Reason,
+        Bank: items.Bank,
+        Check_Date: items.Check_Date,
+        Date_Return: items.Return_Date,
+        SlipCode: items.DepoSlip,
+        ORNum: items.Temp_OR,
+        BankAccnt: items.Bank_Account,
+        nSort: (index + 1).toString().padStart(2, "00"),
+        Date_Collect: new Date(items.OR_Date),
+        Temp_RCNo: `${req.body.RefNo}${(
+          parseInt(req.body.RefNo.split("-")[1]) + index
+        )
+          .toString()
+          .padStart(2, "0")}`,
+      });
+
+      await updatePDCFromReturnCheck(items.Check_No);
+      await updateJournalFromReturnCheck(items.Check_No, items.DepoSlip);
+    });
+
+    await deleteJournalFromReturnCheck(req.body.RefNo);
+    req.body.accountingEntry.forEach(async (items: any) => {
+      await addJournalFromReturnCheck({
+        Branch_Code: req.body.BranchCode,
+        Date_Entry: req.body.DateReturn,
+        Source_Type: "RC",
+        Source_No: req.body.RefNo,
+        Explanation: req.body.Explanation,
+        GL_Acct: items.Code,
+        cGL_Acct: items.AccountName,
+        Sub_Acct: items.SubAcct,
+        cSub_Acct: items.SubAcctName,
+        ID_No: items.IDNo,
+        cID_No: items.IDNo,
+        Debit: parseFloat(items.Debit.replace(/,/g, "")).toFixed(2),
+        Credit: parseFloat(items.Credit.replace(/,/g, "")).toFixed(2),
+        Check_Date: items.Check_Date,
+        Check_No: items.Check_No,
+        Check_Bank: items.Bank,
+        Check_Return: items.Check_Return,
+        Check_Deposit: items.DepoDate,
+        Check_Collect: items.Date_Collection,
+        Check_Reason: items.Check_Reason,
+        TC: "RTC",
+        Source_No_Ref_ID: "",
+      });
+    });
+    await updateRCID(req.body.RefNo.split("-")[1]);
+
+    res.send({
+      message: "Successfully add return check",
+      success: true,
+    });
+  } catch (error: any) {
+    console.log(error.message);
     res.send({ message: error.message, success: false, credit: [], debit: [] });
   }
 });
--- src/model/Task/Accounting/return-checks.model.ts
+++ src/model/Task/Accounting/return-checks.model.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 export async function GenerateReturnCheckID() {
   return await prisma.$queryRawUnsafe(`
     SELECT 
-      concat(a.year,'.', LEFT(a.last_count ,length(a.last_count) -length(a.last_count + 1)),a.last_count + 1) as return_check_id   
+      concat(DATE_FORMAT(NOW(), '%y%m'),'-', LEFT(a.last_count ,length(a.last_count) -length(a.last_count + 1)),a.last_count + 1) as return_check_id   
     FROM
       upward_insurance.id_sequence a
     WHERE
@@ -17,7 +17,7 @@ export async function getCheckList(search: string) {
       DATE_FORMAT(a.Temp_SlipDate, '%m/%d/%Y') AS DepoDate,  
       a.Check_No , 
       a.Check_Date , 
-      FORMAT(a.Credit, 2) AS Amount, 
+      a.Credit as Amount,
       a.Bank, 
       c.Official_Receipt, 
       DATE_FORMAT(c.Date_OR, '%m/%d/%Y') AS Date_OR, 
@@ -43,7 +43,7 @@ export async function getCheckList(search: string) {
 export async function getCreditOnSelectedCheck(BankAccount: string) {
   return await prisma.$queryRawUnsafe(`
   SELECT 
-    a.Account_ID, b.Short, a.IDNo
+    a.Account_ID, b.Acct_Title,  b.Short, a.IDNo
 FROM
     upward_insurance.bankaccounts a
         LEFT JOIN
@@ -77,4 +77,52 @@ export async function getBranchName() {
   return await prisma.$queryRawUnsafe(
     `SELECT a.ShortName FROM upward_insurance.sub_account a where a.Acronym = 'HO'`
   );
+}
+
+export async function deleteReturnCheck(RC_No: string) {
+  return await prisma.$queryRawUnsafe(`
+    delete from upward_insurance.return_checks where RC_NO='${RC_No}'
+  `);
+}
+
+export async function addNewReturnCheck(data: any) {
+  return await prisma.return_checks.create({ data });
+}
+
+export async function updatePDCFromReturnCheck(Check_No: string) {
+  return await prisma.$queryRawUnsafe(`
+    UPDATE upward_insurance.pdc a SET a.SlipCode ='', a.ORNum='' WHERE  a.Check_No ='${Check_No}' 
+  `);
+}
+
+export async function updateJournalFromReturnCheck(
+  Check_No: string,
+  SlipCode: string
+) {
+  return await prisma.$queryRawUnsafe(`
+    UPDATE upward_insurance.journal a 
+    SET a.TC ='RTC'  
+    WHERE  a.Check_No ='${Check_No}' AND a.Source_No = '${SlipCode}' AND a.Source_Type ='OR'
+  `);
+}
+
+export async function deleteJournalFromReturnCheck(SlipCode: string) {
+  return await prisma.$queryRawUnsafe(`
+  DELETE FROM upward_insurance.journal a WHERE a.Source_No = '${SlipCode}' AND a.Source_Type = 'RC'`);
+}
+export async function addJournalFromReturnCheck(data: any) {
+  return await prisma.journal.create({ data });
+}
+
+export async function updateRCID(last_count: string) {
+  return await prisma.$queryRawUnsafe(`
+  UPDATE upward_insurance.id_sequence a 
+    SET 
+        a.last_count = '${last_count}',
+        a.year = DATE_FORMAT(NOW(), '%y'),
+        month = DATE_FORMAT(NOW(), '%m')
+    WHERE
+        a.type = 'return-check'
+  
+  `);
 }
