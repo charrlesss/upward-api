0553bd1dbb8a9c493d4170748a3b74fb81dcc842
charrlesss <charlespalencia21@gmail.com>
2024-02-29 17:25:21 +0800

    postponement

--- src/controller/Task/Accounting/chek-postponement.ts
+++ src/controller/Task/Accounting/chek-postponement.ts
@@ -1,9 +1,22 @@
 import express from "express";
 import {
+  approvalCodePostponement,
   checkPostponementRequestAutoID,
+  createPostponement,
+  createPostponementDetails,
   getCheckPostponementPNNo,
   getSelectedCheckPostponementPNNo,
+  searchEditPostponentRequest,
+  searchSelectedEditPostponentRequest,
+  updateOnCancelPostponentRequest,
+  updateOnCancelPostponentRequestDetails,
 } from "../../../model/Task/Accounting/chek-postponement.model";
+import { getUserById } from "../../../model/StoredProcedure";
+import { updateAnyId } from "../../../model/Task/Accounting/pullout.model";
+import { format } from "date-fns";
+import sendEmail from "../../../lib/sendEmail";
+import generateRandomNumber from "../../../lib/generateRandomNumber";
+
 const CheckPostponement = express.Router();
 
 CheckPostponement.get(
@@ -11,7 +24,7 @@ CheckPostponement.get(
   async (req, res) => {
     try {
       res.send({
-        message: "Successfully",
+        message: "Successfully Get ID",
         success: true,
         id: await checkPostponementRequestAutoID(),
       });
@@ -27,7 +40,11 @@ CheckPostponement.get("/check-postponement/pn-no", async (req, res) => {
     const clientCheckName = await getCheckPostponementPNNo(
       pnclientSearch as string
     );
-    res.send({ message: "SERVER ERROR!", success: true, clientCheckName });
+    res.send({
+      message: "Successfully Search",
+      success: true,
+      clientCheckName,
+    });
   } catch (error: any) {
     console.log(`${CheckPostponement} : ${error.message}`);
     res.send({ message: "SERVER ERROR!", success: false, clientCheckName: [] });
@@ -36,13 +53,17 @@ CheckPostponement.get("/check-postponement/pn-no", async (req, res) => {
 CheckPostponement.get(
   "/check-postponement/selected-pn-no",
   async (req, res) => {
-    const { check ,pnno } = req.query;
+    const { check, pnno } = req.query;
     try {
-        const selectedChecks = await getSelectedCheckPostponementPNNo(
-            pnno as string,
-            check as string,
-        );
-      res.send({ message: "SERVER ERROR!", success: true, selectedChecks });
+      const selectedChecks = await getSelectedCheckPostponementPNNo(
+        pnno as string,
+        check as string
+      );
+      res.send({
+        message: "Successfully Get Search Selected",
+        success: true,
+        selectedChecks,
+      });
     } catch (error: any) {
       console.log(`${CheckPostponement} : ${error.message}`);
       res.send({
@@ -53,5 +74,335 @@ CheckPostponement.get(
     }
   }
 );
+CheckPostponement.post("/check-postponement/save", async (req, res) => {
+  try {
+    const user = await getUserById((req.user as any).UserId);
+    const data = {
+      RPCDNo: req.body.RPCD,
+      PNNo: req.body.PNNo,
+      HoldingFees: req.body.holdingFee,
+      PenaltyCharge: req.body.penaltyCharge,
+      PaidVia: req.body.paidVia,
+      PaidInfo: req.body.paidInfo,
+      PaidDate: req.body.paidDate,
+      Date: new Date(),
+      Status: "PENDING",
+      Branch: "HO",
+      ClientBranch: req.body.branch,
+      Prepared_by: user?.Username,
+      Surplus: req.body.surplus,
+      Deducted_to: req.body.deductedTo,
+    };
+    await createPostponement(data);
+    JSON.parse(req.body.checkSelected).forEach(async (item: any) => {
+      const details = {
+        RPCD: req.body.RPCD,
+        RPCDNo: `${req.body.RPCD}-${item.temp_id}`,
+        CheckNo: item.Check_No,
+        OldCheckDate: new Date(item.Check_Date),
+        NewCheckDate: new Date(item.New_Check_Date),
+        Reason: item.Reason,
+      };
+      await createPostponementDetails(details);
+    });
+    const subtitle = `
+      <h3>Check Deposit Postponement Request</h3>
+    `;
+    const text = getSelectedCheck(req.body.checkSelected);
+    const Requested_By = user?.Username;
+    const Requested_Date = new Date();
+    const approvalCode = generateRandomNumber(6);
+
+    await sendRequestEmail({
+      ...req.body,
+      text,
+      Requested_By,
+      Requested_Date,
+      approvalCode,
+      subtitle,
+    });
+    await approvalCodePostponement({
+      RPCD: req.body.RPCD,
+      For_User: Requested_By,
+      Approved_Code: approvalCode.toString(),
+      Disapproved_Code: "",
+    });
+    await updateAnyId("check-postponement");
+    res.send({ message: "Save Successfully.", success: true });
+  } catch (error: any) {
+    console.log(`${CheckPostponement} : ${error.message}`);
+    res.send({ message: "SERVER ERROR!", success: false });
+  }
+});
+CheckPostponement.get("/check-postponement/search-edit", async (req, res) => {
+  const { searchEdit } = req.query;
+  try {
+    const selectedRequest = await searchEditPostponentRequest(
+      searchEdit as string
+    );
+    res.send({
+      message: "Successfully Get Search Selected",
+      success: true,
+      selectedRequest,
+    });
+  } catch (error: any) {
+    console.log(`${CheckPostponement} : ${error.message}`);
+    res.send({
+      message: "SERVER ERROR!",
+      success: false,
+      selectedRequest: [],
+    });
+  }
+});
+CheckPostponement.post(
+  "/check-postponement/search-selected-edit",
+  async (req, res) => {
+    try {
+      const selectedSearchEdit = await searchSelectedEditPostponentRequest(
+        req.body.RPCD
+      );
+      res.send({
+        message: "Successfully Get Search Selected",
+        success: true,
+        selectedSearchEdit,
+      });
+    } catch (error: any) {
+      console.log(`${CheckPostponement} : ${error.message}`);
+      res.send({
+        message: "SERVER ERROR!",
+        success: false,
+        selectedSearchEdit: [],
+      });
+    }
+  }
+);
+CheckPostponement.post(
+  "/check-postponement/cancel-request",
+  async (req, res) => {
+    try {
+      await updateOnCancelPostponentRequest(req.body.RPCD);
+      await updateOnCancelPostponentRequestDetails(req.body.RPCD);
+      res.send({
+        message: `Cancel Request ${req.body.RPCD} Successfully`,
+        success: true,
+      });
+    } catch (error: any) {
+      console.log(`${CheckPostponement} : ${error.message}`);
+      res.send({
+        message: "SERVER ERROR!",
+        success: false,
+      });
+    }
+  }
+);
+function getSelectedCheck(selected: string) {
+  let tbodyText = "";
+  JSON.parse(selected).forEach((item: any) => {
+    tbodyText += generateTextTable(item);
+  });
+  return tbodyText;
+}
+function generateTextTable(item: any) {
+  return `
+  <tr>
+    <td style="border: 1px solid #ddd; padding: 8px">${item.Check_No}</td>
+    <td style="border: 1px solid #ddd; padding: 8px">${item.Bank}</td>
+    <td style="border: 1px solid #ddd; padding: 8px">₱${item.Check_Amnt}</td>
+    <td style="border: 1px solid #ddd; padding: 8px">${item.Check_Date}</td>
+    <td style="border: 1px solid #ddd; padding: 8px">${item.New_Check_Date}</td>
+    <td style="border: 1px solid #ddd; padding: 8px">${item.DateDiff}</td>
+    <td style="border: 1px solid #ddd; padding: 8px">${item.Reason}</td>
+  </tr>`;
+}
+async function sendRequestEmail(props: any) {
+  const {
+    RPCD,
+    PNNo,
+    client,
+    text,
+    Requested_Date,
+    Requested_By,
+    approvalCode,
+    subtitle,
+    holdingFee,
+    penaltyCharge,
+    surplus,
+    paidVia,
+  } = props;
+  const totalFee =
+    parseFloat(holdingFee.replace(/,/g, "")) +
+    parseFloat(penaltyCharge.replace(/,/g, "")) +
+    parseFloat(surplus.replace(/,/g, ""));
+  const strong1 = `
+    font-family: Arial, Helvetica, sans-serif;
+            font-size: 14px;
+            color: #334155;
+          
+    `;
+  const strong2 = `font-family: 'Courier New', Courier, monospace;
+    font-size: 16px;`;
+  const th = ` border: 1px solid #ddd;
+    padding: 8px;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #64748b;
+    color: white;`;
+  await sendEmail(
+    { user: "upwardinsurance.gelo@gmail.com", pass: "onss clqu vwnp tbea" },
+    "upwardinsurance.gelo@gmail.com",
+    "charlespalencia21@gmail.com",
+    `
+  <div
+    style="
+      background-color: #64748b;
+      color: white;
+      padding: 7px;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      text-align: center;
+    "
+  >
+    <h2>UPWARD</h2>
+    ${subtitle}
+    
+  </div>
+  <div style="text-align: center">
+    <p>
+      <strong
+        style="${strong1}"
+        >RCPD No. : </strong
+      ><strong
+        style="${strong2}"
+        >${RPCD}</strong
+      >
+    </p>
+    <p>
+      <strong
+        style="${strong1}"
+        >PN No. : </strong
+      ><strong
+        style="${strong2}"
+        >${PNNo}</strong
+      >
+    </p>
+    <p>
+      <strong
+        style="${strong1}"
+        >Branch : </strong
+      ><strong
+        style="${strong2}"
+        >HO</strong
+      >
+    </p>
+    <p>
+    <strong
+      style="${strong1}"
+      >Account Name : </strong
+    ><strong
+      style="${strong2}"
+      >${client}</strong
+    >
+    ${
+      approvalCode
+        ? `<p>
+      <strong
+        style="${strong1}"
+        >Approval Code : </strong
+      ><strong
+        style="${strong2}"
+        >${approvalCode}</strong
+      >
+    </p>`
+        : ""
+    }
+  </div>
+  <table
+    style="
+      font-family: Arial, Helvetica, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    "
+  >
+    <thead>
+      <tr>
+        <th
+          style="${th}"
+        >
+          Check No.
+        </th>
+        <th
+          style="${th}"
+        >
+          Bank
+        </th>
+        <th
+          style="${th}"
+        >
+          Amount
+        </th>
+        <th
+          style="${th}"
+        >
+          Old Deposite Date
+        </th>
+        <th
+          style="${th}"
+        >
+          New Deposite Date
+        </th>
+        <th
+          style="${th}"
+        >
+          Date Difference
+        </th>
+        <th
+          style="${th}"
+        >
+          Reason
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      ${text}
+    </tbody>
+  </table>
+  <br />
+  <br />
+  <div
+    style="
+      text-align: center;
+      font-size: 14px;
+      font-family: 'Courier New', Courier, monospace;
+    "
+  >
+    <p>Total Fees:<span style="font-weight: 600; color: #334155;">${totalFee}</span></p>
+    <p style="font-weight: 200">
+    How to be paid:<span style="font-weight: 600;color: #334155;">
+      ${paidVia}
+    </span>
+    </p>
+    <p>Other Informations</p>
+  </div>
+  <br />
+  <br />
+  <div
+    style="
+      text-align: center;
+      font-size: 14px;
+      font-family: 'Courier New', Courier, monospace;
+    "
+  >
+    <p>Request By:<span style="font-weight: 600; color: #334155;">${Requested_By}</span></p>
+    <p style="font-weight: 200">
+      Request Date:<span style="font-weight: 600;color: #334155;">${format(
+        Requested_Date,
+        "MM/dd/yyyy"
+      )}</span>
+    </p>
+    <p>This is a computer generated E-mail</p>
+  </div>
+    `
+  );
+}
 
 export default CheckPostponement;
--- src/model/Task/Accounting/chek-postponement.model.ts
+++ src/model/Task/Accounting/chek-postponement.model.ts
@@ -89,21 +89,103 @@ export async function getCheckPostponementPNNo(search: string) {
     `;
   return await prisma.$queryRawUnsafe(query);
 }
-export async function getSelectedCheckPostponementPNNo(PNNo: String,checkNo:String) {
+export async function getSelectedCheckPostponementPNNo(
+  PNNo: string,
+  checkNo: string
+) {
   const query = `
       SELECT 
           PDC_ID,
           date_format(Check_Date , '%m/%d/%Y') as Check_Date,
           Bank,
           Check_No,
-          Check_Amnt
+          Check_Amnt,
+          ifnull(Status,'--') as Status
       FROM
           upward_insurance.PDC PD
+          left join (
+            SELECT bb.CheckNo,aa.Status FROM upward_insurance.postponement aa
+                left join upward_insurance.postponement_detail bb on aa.RPCDNo = bb.RPCD and bb.cancel = 0 and  aa.Status <> 'CANCEL'
+                ) b on PD.Check_No = b.CheckNo 
       WHERE
         PNo = '${PNNo}'
         AND PDC_Status = 'Stored'
         AND Check_No like '%${checkNo}%'
-      ORDER BY Check_No
+        ORDER BY Check_No
     ;`;
   return await prisma.$queryRawUnsafe(query);
+}
+export async function searchEditPostponentRequest(search: string) {
+  const query = `
+  SELECT 
+  RPCDNo,
+  PNNo,
+  Deducted_to,
+  ClientBranch,
+  format(HoldingFees,2) AS holdingFee,
+  format(PenaltyCharge,2) AS penaltyCharge,
+  format(Surplus,2) AS surplus,
+  Deducted_to AS deductedTo,
+  PaidVia AS paidVia,
+  PaidInfo AS paidInfo,
+  date_format(PaidDate,'%Y-%m-%d') AS paidDate,
+  format(CAST(REPLACE(HoldingFees, ',', '') AS DECIMAL) + CAST(REPLACE(PenaltyCharge, ',', '') AS DECIMAL) + CAST(REPLACE(Surplus, ',', '') AS DECIMAL),2) AS total
+FROM
+  upward_insurance.postponement a
+WHERE
+  Status <> 'CANCEL' AND
+  (
+    RPCDNo LIKE '%${search}%' OR 
+    PNNo LIKE '%${search}%' OR 
+    Deducted_to LIKE '%${search}%'
+  )
+  `;
+  return await prisma.$queryRawUnsafe(query);
+}
+export async function searchSelectedEditPostponentRequest(RPCD: string) {
+  const query = `
+    SELECT 
+        b.Check_No,
+        b.Bank,
+        b.Check_Amnt,
+        DATE_FORMAT(b.Check_Date, '%Y-%m-%d') AS Check_Date,
+        DATE_FORMAT(a.NewCheckDate, '%Y-%m-%d') AS New_Check_Date,
+        ABS(DATEDIFF(a.NewCheckDate, b.Check_Date)) AS DateDiff,
+        a.Reason,
+        c.Status,
+        LPAD(ROW_NUMBER() OVER (), 3, '0') as temp_id
+    FROM
+        upward_insurance.postponement_detail a
+            LEFT JOIN
+        upward_insurance.pdc b ON a.CheckNo = b.Check_No
+        LEFT JOIN 
+        upward_insurance.postponement c on a.RPCD = c.RPCDNo
+    WHERE
+      RPCD = '${RPCD}' AND
+      a.cancel = 0 AND
+      c.Status <> 'CANCEL'
+    ;`;
+  return await prisma.$queryRawUnsafe(query);
+}
+export async function updateOnCancelPostponentRequest(RPCD: string) {
+  const query = `
+    update  upward_insurance.postponement a set a.Status = 'CANCEL'  where a.RPCDNo = '${RPCD}'
+    ;`;
+  return await prisma.$queryRawUnsafe(query);
+}
+
+export async function updateOnCancelPostponentRequestDetails(RPCD: string) {
+  const query = `
+    update  upward_insurance.postponement_detail a set a.cancel = 1  where a.RPCD = '${RPCD}'
+    ;`;
+  return await prisma.$queryRawUnsafe(query);
+}
+export async function createPostponement(data: any) {
+  return await prisma.postponement.create({ data });
+}
+export async function createPostponementDetails(data: any) {
+  return await prisma.postponement_detail.create({ data });
+}
+export async function approvalCodePostponement(data: any) {
+  return await prisma.postponement_auth_codes.create({ data });
 }
