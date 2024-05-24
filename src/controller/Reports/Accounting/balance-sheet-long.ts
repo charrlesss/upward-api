40305052629f07fa356d832e0a99e34493da5e42
charrlesss <charlespalencia21@gmail.com>
2024-05-24 17:09:53 +0800

    f

--- src/controller/Reports/Accounting/balance-sheet-long.ts
+++ src/controller/Reports/Accounting/balance-sheet-long.ts
@@ -6,18 +6,6 @@ const BalanceSheetLong = express.Router();
 const prisma = new PrismaClient();
 
 BalanceSheetLong.post("/balance-sheet-long-report", async (req, res) => {
-  console.log(req.body);
-
-  //   {
-  //     dateFormat: 'Daily',
-  //     format: 0,
-  //     date: '2024-05-03T05:09:35.550Z',
-  //     sub_acct: 'All',
-  //     title: 'UPWARD MANAGEMENT INSURANCE SERVICES\n' +
-  //       'Daily Balance Sheet - Long\n' +
-  //       'May 3, 2024',
-  //     nominalAccount: 0
-  //   }
   let qry = "";
   if (req.body.format === 0) {
     const tmp = `
@@ -121,11 +109,11 @@ BalanceSheetLong.post("/balance-sheet-long-report", async (req, res) => {
       HT2,
       H3,
       HT3,
-      format(PrevBalance,0) as PrevBalance,
-      format(CurrDebit,0) as CurrDebit,
-      format(CurrCredit,0) as CurrCredit,
-      format(CurrBalance,0) as CurrBalance,
-      format(TotalBalance,0) as TotalBalance,
+      format(ifnull(PrevBalance,0),0) as PrevBalance,
+      format(ifnull(CurrDebit,0),0) as CurrDebit,
+      format(ifnull(CurrCredit,0),0) as CurrCredit,
+      format(ifnull(CurrBalance,0),0) as CurrBalance,
+      format(ifnull(TotalBalance,0),0) as TotalBalance,
       CASE WHEN CAST(H1 AS UNSIGNED) < 4 THEN 'ASSETS' ELSE 'LIABILITIES' END AS H 
     FROM (${finals}) Final`;
   } else {
@@ -225,22 +213,123 @@ BalanceSheetLong.post("/balance-sheet-long-report", async (req, res) => {
       HT2,
       H3,
       HT3,
-      format(PrevBalance,0) as PrevBalance,
-      format(CurrDebit,0) as CurrDebit,
-      format(CurrCredit,0) as CurrCredit,
-      format(CurrBalance,0) as CurrBalance,
-      format(TotalBalance,0) as TotalBalance,
+      format(ifnull(PrevBalance,0),0) as PrevBalance,
+      format(ifnull(CurrDebit,0),0) as CurrDebit,
+      format(ifnull(CurrCredit,0),0) as CurrCredit,
+      format(ifnull(CurrBalance,0),0) as CurrBalance,
+      format(ifnull(TotalBalance,0),0) as TotalBalance,
       CASE WHEN CAST(H1 AS UNSIGNED) < 4 THEN 'ASSETS' ELSE 'LIABILITIES' END AS H 
     FROM (${Finals}) Final`;
   }
-  console.log(qry);
   const data: any = await prisma.$queryRawUnsafe(qry);
-  const assetArray = data.filter((itm: any) => itm.H === "ASSETS");
-  assetArray.unshift({
+  const groupByCategory = (data: any, datakeyGroup: any) => {
+    return data.reduce((acc: any, item: any) => {
+      (acc[item[datakeyGroup]] = acc[item[datakeyGroup]] || []).push(item);
+      return acc;
+    }, {});
+  };
+  const Assets: any = [];
+  const ASSETS = data.filter((itm: any) => itm.H === "ASSETS");
+  const assetHT1 = groupByCategory(ASSETS, "HT1");
+  const keyAsset = Object.keys(assetHT1);
+  keyAsset.forEach((k2) => {
+    Assets.push({
+      N: k2,
+      H1: "",
+      HT1: k2,
+      H2: "",
+      H3: "",
+      HT3: "",
+      PrevBalance: "",
+      CurrDebit: "",
+      CurrCredit: "",
+      CurrBalance: "",
+      TotalBalance: "",
+      H: "",
+      HT2: "",
+      h2: true,
+    });
+    const groupData3 = groupByCategory(assetHT1[k2], "HT2");
+    const keys = Object.keys(groupData3);
+    keys.forEach((k) => {
+      Assets.push({
+        N: k,
+        H1: "",
+        HT1: "",
+        H2: "",
+        H3: "",
+        HT3: "",
+        PrevBalance: "",
+        CurrDebit: "",
+        CurrCredit: "",
+        CurrBalance: "",
+        TotalBalance: "",
+        H: "",
+        HT2: k,
+        h3: true,
+      });
+      groupData3[k].forEach((data: any) => {
+        Assets.push({ ...data, N: data.HT3, h4: true });
+      });
+      Assets.push({
+        borderTop: true,
+        border: false,
+        N: "",
+        H1: "",
+        HT1: "",
+        H2: "",
+        H3: "",
+        HT3: "",
+        PrevBalance: formatNumber(getTotal(groupData3[k], "PrevBalance")),
+        CurrDebit: formatNumber(getTotal(groupData3[k], "CurrDebit")),
+        CurrCredit: formatNumber(getTotal(groupData3[k], "CurrCredit")),
+        CurrBalance: formatNumber(getTotal(groupData3[k], "CurrBalance")),
+        TotalBalance: formatNumber(getTotal(groupData3[k], "TotalBalance")),
+        H: "",
+        HT2: k,
+        h3: true,
+      });
+    });
+    Assets.push({
+      border: true,
+      N: `TOTAL ${k2}`,
+      H1: "",
+      HT1: k2,
+      H2: "",
+      H3: "",
+      HT3: "",
+      PrevBalance: formatNumber(getTotal(assetHT1[k2], "PrevBalance")),
+      CurrDebit: formatNumber(getTotal(assetHT1[k2], "CurrDebit")),
+      CurrCredit: formatNumber(getTotal(assetHT1[k2], "CurrCredit")),
+      CurrBalance: formatNumber(getTotal(assetHT1[k2], "CurrBalance")),
+      TotalBalance: formatNumber(getTotal(assetHT1[k2], "TotalBalance")),
+      H: "",
+      HT2: "",
+      h2: true,
+    });
+  });
+  Assets.push({
+    border: true,
+    N: "TOTAL ASSETS",
     H1: "",
-    HT1: "TOTAL ASSET",
+    HT1: "",
     H2: "",
+    H3: "",
+    HT3: "",
+    PrevBalance: formatNumber(getTotal(ASSETS, "PrevBalance")),
+    CurrDebit: formatNumber(getTotal(ASSETS, "CurrDebit")),
+    CurrCredit: formatNumber(getTotal(ASSETS, "CurrCredit")),
+    CurrBalance: formatNumber(getTotal(ASSETS, "CurrBalance")),
+    TotalBalance: formatNumber(getTotal(ASSETS, "TotalBalance")),
+    H: "ASSETS",
     HT2: "",
+    h1: true,
+  });
+  Assets.unshift({
+    N: "ASSETS",
+    H1: "",
+    HT1: "",
+    H2: "",
     H3: "",
     HT3: "",
     PrevBalance: "",
@@ -248,31 +337,131 @@ BalanceSheetLong.post("/balance-sheet-long-report", async (req, res) => {
     CurrCredit: "",
     CurrBalance: "",
     TotalBalance: "",
-    H: "",
-  });
-  assetArray.push({
-    H1: "",
-    HT1: "ASSET",
-    H2: "",
+    H: "ASSETS",
     HT2: "",
+    h1: true,
+  });
+  const Liabilities: any = [];
+  const LIABILITIES = data.filter((itm: any) => itm.H === "LIABILITIES");
+  const liabilitiesHT1 = groupByCategory(LIABILITIES, "HT1");
+  const keyLiabilities = Object.keys(liabilitiesHT1);
+  keyLiabilities.forEach((k2) => {
+    Liabilities.push({
+      N: k2,
+      H1: "",
+      HT1: k2,
+      H2: "",
+      H3: "",
+      HT3: "",
+      PrevBalance: "",
+      CurrDebit: "",
+      CurrCredit: "",
+      CurrBalance: "",
+      TotalBalance: "",
+      H: "",
+      HT2: "",
+      h2: true,
+    });
+    const groupData3 = groupByCategory(liabilitiesHT1[k2], "HT2");
+    const keys = Object.keys(groupData3);
+    keys.forEach((k) => {
+      Liabilities.push({
+        N: "",
+        H1: "",
+        HT1: "",
+        H2: "",
+        H3: "",
+        HT3: "",
+        PrevBalance: "",
+        CurrDebit: "",
+        CurrCredit: "",
+        CurrBalance: "",
+        TotalBalance: "",
+        H: "",
+        HT2: k,
+        h3: true,
+      });
+      groupData3[k].forEach((data: any) => {
+        Liabilities.push({ ...data, N: data.HT3, h4: true });
+      });
+      Liabilities.push({
+        borderTop: true,
+        border: false,
+        N: `TOTAL ${k}`,
+        H1: "",
+        HT1: "",
+        H2: "",
+        H3: "",
+        HT3: "",
+        PrevBalance: formatNumber(getTotal(groupData3[k], "PrevBalance")),
+        CurrDebit: formatNumber(getTotal(groupData3[k], "CurrDebit")),
+        CurrCredit: formatNumber(getTotal(groupData3[k], "CurrCredit")),
+        CurrBalance: formatNumber(getTotal(groupData3[k], "CurrBalance")),
+        TotalBalance: formatNumber(getTotal(groupData3[k], "TotalBalance")),
+        H: "",
+        HT2: k,
+        h3: true,
+      });
+    });
+
+    Liabilities.push({
+      border: true,
+      N: `TOTAL ${k2}`,
+      H1: "",
+      HT1: k2,
+      H2: "",
+      H3: "",
+      HT3: "",
+      PrevBalance: formatNumber(getTotal(liabilitiesHT1[k2], "PrevBalance")),
+      CurrDebit: formatNumber(getTotal(liabilitiesHT1[k2], "CurrDebit")),
+      CurrCredit: formatNumber(getTotal(liabilitiesHT1[k2], "CurrCredit")),
+      CurrBalance: formatNumber(getTotal(liabilitiesHT1[k2], "CurrBalance")),
+      TotalBalance: formatNumber(getTotal(liabilitiesHT1[k2], "TotalBalance")),
+      H: "",
+      HT2: "",
+      h2: true,
+    });
+  });
+
+  Liabilities.push({
+    border: true,
+    N: "TOTAL LIABILITIES AND CAPITAL",
+    H1: "",
+    HT1: "",
+    H2: "",
     H3: "",
     HT3: "",
-    PrevBalance: formatNumber(getTotal(assetArray, "PrevBalance")),
-    CurrDebit: formatNumber(getTotal(assetArray, "CurrDebit")),
-    CurrCredit: formatNumber(getTotal(assetArray, "CurrCredit")),
-    CurrBalance: formatNumber(getTotal(assetArray, "CurrBalance")),
-    TotalBalance: formatNumber(getTotal(assetArray, "TotalBalance")),
-    H: "",
+    PrevBalance: formatNumber(getTotal(LIABILITIES, "PrevBalance")),
+    CurrDebit: formatNumber(getTotal(LIABILITIES, "CurrDebit")),
+    CurrCredit: formatNumber(getTotal(LIABILITIES, "CurrCredit")),
+    CurrBalance: formatNumber(getTotal(LIABILITIES, "CurrBalance")),
+    TotalBalance: formatNumber(getTotal(LIABILITIES, "TotalBalance")),
+    H: "LIABILITIES",
+    HT2: "",
+    h1: true,
   });
-  // const liabilitiesArray = data.filter((itm: any) => itm.H === "LIABILITIES");
-  // const assetArrayBody = arrangeArray(assetArray, "HT2");
-  // const liabilitiesArrayBody = arrangeArray(liabilitiesArray, "HT2");
-
+  Liabilities.unshift({
+    N: "LIABILITIES",
+    H1: "",
+    HT1: "",
+    H2: "",
+    H3: "",
+    HT3: "",
+    PrevBalance: "",
+    CurrDebit: "",
+    CurrCredit: "",
+    CurrBalance: "",
+    TotalBalance: "",
+    H: "LIABILITIES",
+    HT2: "",
+    h1: true,
+  });
+  const report = Assets.concat(Liabilities);
   try {
     res.send({
       message: "Successfully ger report",
       success: true,
-      assetArray,
+      report,
     });
   } catch (err: any) {
     console.log(err.message);
@@ -284,152 +473,20 @@ BalanceSheetLong.post("/balance-sheet-long-report", async (req, res) => {
   }
 });
 
-function formatNumber(number:number){
- return number.toLocaleString("en-US", {
+function formatNumber(number: number) {
+  return number.toLocaleString("en-US", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  })
+  });
 }
 function getTotal(array: Array<any>, datakey: string) {
   return array.reduce((d: any, itms: any) => {
-    return (
-      d +
-      Math.abs(
-        parseFloat(Math.abs(itms[datakey]).toString()?.replace(/,/g, ""))
-      )
-    );
+    let num = parseFloat(itms[datakey].toString()?.replace(/,/g, ""));
+    if (isNaN(num)) {
+      num = 0;
+    }
+    return d + Math.abs(num);
   }, 0);
 }
-
-// function arrangeArray(dataArray: Array<any>, datakey: string) {
-//   const groupedDataIncome = dataArray.reduce((acc: any, obj: any) => {
-//     const key = obj[datakey];
-//     if (!acc[key]) {
-//       acc[key] = [];
-//     }
-//     acc[key].push(obj);
-//     return acc;
-//   }, {});
-
-//   for (const group in groupedDataIncome) {
-//     const groupTotal = groupedDataIncome[group].reduce(
-//       (acc: any, obj: any) => {
-//         acc.PrevBalance += Math.abs(
-//           parseFloat(obj.PrevBalance?.toString().replace(/,/g, "") || 0)
-//         );
-//         acc.CurrDebit += Math.abs(
-//           parseFloat(obj.CurrDebit?.toString().replace(/,/g, "") || 0)
-//         );
-//         acc.CurrCredit += Math.abs(
-//           parseFloat(obj.CurrCredit?.toString().replace(/,/g, "") || 0)
-//         );
-//         acc.CurrBalance += Math.abs(
-//           parseFloat(obj.CurrBalance?.toString().replace(/,/g, "") || 0)
-//         );
-//         acc.TotalBalance += Math.abs(
-//           parseFloat(obj.TotalBalance?.toString().replace(/,/g, "") || 0)
-//         );
-//         return acc;
-//       },
-//       {
-//         PrevBalance: 0,
-//         CurrDebit: 0,
-//         CurrCredit: 0,
-//         CurrBalance: 0,
-//         TotalBalance: 0,
-//       }
-//     );
-//     const summaryObjectFirst: any = {
-//       H1: "",
-//       HT1: groupedDataIncome[group][0].HT1,
-//       H2: "",
-//       HT2: groupedDataIncome[group][0].HT2,
-//       H3: "",
-//       HT3: "",
-//       PrevBalance: "",
-//       CurrDebit: "",
-//       CurrCredit: "",
-//       CurrBalance: "",
-//       TotalBalance: "",
-//       H: "",
-//     };
-//     summaryObjectFirst[datakey] = group;
-//     groupedDataIncome[group].unshift(summaryObjectFirst);
-//     const summaryObjectLast: any = {
-//       H1: "",
-//       HT1: groupedDataIncome[group][0].HT1,
-//       H2: "",
-//       HT2: groupedDataIncome[group][0].HT2,
-//       H3: "",
-//       HT3: "",
-//       PrevBalance: groupTotal.PrevBalance.toString(),
-//       CurrDebit: groupTotal.CurrDebit.toString(),
-//       CurrCredit: groupTotal.CurrCredit.toString(),
-//       CurrBalance: groupTotal.CurrBalance.toString(),
-//       TotalBalance: groupTotal.TotalBalance.toString(),
-//       H: "",
-//     };
-//     summaryObjectLast[datakey] = group;
-//     groupedDataIncome[group].push(summaryObjectLast);
-//   }
-
-//   const r = Object.values(groupedDataIncome);
-//   r.forEach((d: any) => {
-//     let PrevBalance = 0;
-//     let CurrDebit = 0;
-//     let CurrCredit = 0;
-//     let CurrBalance = 0;
-//     let TotalBalance = 0;
-
-//     d.forEach((data: any, idx: number) => {
-//       if (idx === d.length - 1) {
-//         PrevBalance += Math.abs(
-//           parseFloat(data.PrevBalance.toString().replace(/,/g, "") || 0)
-//         );
-//         CurrDebit += Math.abs(
-//           parseFloat(data.CurrDebit.toString().replace(/,/g, "") || 0)
-//         );
-//         CurrCredit += Math.abs(
-//           parseFloat(data.CurrCredit.toString().replace(/,/g, "") || 0)
-//         );
-//         CurrBalance += Math.abs(
-//           parseFloat(data.CurrBalance.toString().replace(/,/g, "") || 0)
-//         );
-//         TotalBalance += Math.abs(
-//           parseFloat(data.TotalBalance.toString().replace(/,/g, "") || 0)
-//         );
-//       }
-//     });
-//     d.push({
-//       H1: "",
-//       HT1: d[0].HT1,
-//       H2: "",
-//       HT2: d[0].HT2,
-//       H3: "",
-//       HT3: "",
-//       PrevBalance,
-//       CurrDebit,
-//       CurrCredit,
-//       CurrBalance,
-//       TotalBalance,
-//       H: "",
-//     });
-//     d.unshift({
-//       H1: "",
-//       HT1: d[0].HT1,
-//       H2: "",
-//       HT2: d[0].HT2,
-//       H3: "",
-//       HT3: "",
-//       PrevBalance: "",
-//       CurrDebit: "",
-//       CurrCredit: "",
-//       CurrBalance: "",
-//       TotalBalance: "",
-//       H: "",
-//     });
-//   });
-//   return Object.values(groupedDataIncome);
-// }
 
 export default BalanceSheetLong;
